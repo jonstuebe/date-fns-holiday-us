ba6a415f1388080d0df176dd56a28c5ebfdefd60
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-06-11 22:59:07 +1000

    build: fix tsdown & package.json entries

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,9 +2,19 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
-  sourcemap: true,
-  clean: true,
+  format: ["commonjs", "esm", "iife"],
+  platform: "node",
+  outDir: "./dist",
   dts: true,
-  target: false,
+  clean: true,
+  minify: true,
+  sourcemap: true,
+  exports: true,
+  cjsDefault: true,
+  outputOptions: {
+    name: "date_fns_holidays",
+    globals: {
+      "date-fns": "date_fns",
+    },
+  },
 });
