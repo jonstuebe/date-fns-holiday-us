bc1445c2bf6398da47a83158055514996602f5a2
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-06-11 22:26:26 +1000

    chore: migrate tsup to tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs", "iife"],
-  legacyOutput: true,
-  splitting: false,
   sourcemap: true,
   clean: true,
   dts: true,
+  target: false,
 });
